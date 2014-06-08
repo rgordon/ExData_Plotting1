278a3b80141aa8233b607ecf0cfa97d1e55b8977
Rick Gordon <rick.gordon@disney.com>
2014-06-07 17:02:09 -0700

    do not need the weekdays after all

--- plot2.R
+++ plot2.R
@@ -2,10 +2,6 @@ source(file="LoadAndPrepInput.R")
 
 png(filename="plot2.png",width=480,height=480)
 
-# determine the weekday for each date, and make Factor for graphic use
-hpc$weekday <- weekdays(hpc$DateTime,abbreviate=TRUE)
-hpc$weekday <- as.factor(hpc$weekday)
-
 with(hpc, plot(x=DateTime, y=Global_active_power,type="l",xlab="", ylab="Global Active Power (kilowatts)"))
 
 dev.off()
